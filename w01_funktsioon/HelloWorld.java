15da3b851de33efc3fc9c7c61606528a367af1f6
uWookies <martten@tlu.ee>
2020-02-04 11:19:23 +0200

    2nd commit

--- w01_funktsioon/HelloWorld.java
+++ w01_funktsioon/HelloWorld.java
@@ -1,11 +1,43 @@
 public class HelloWorld{
 
+    
     public static void main(String[] arg){
+
+        //print lause
         System.out.println("Hello Man");
 
-        //COOL STFF
         /*
-        multiline
+        min value
+        max value
         */
+        byte minByte = Byte.MIN_VALUE;
+        byte maxByte = Byte.MAX_VALUE;
+        // short, int, long, float, double, char, boolean
+
+        Boolean flag = true;
+
+        System.out.println(minByte + maxByte);
+
+        // operaatorid * / - %
+
+        int[] numberArray = new int[]{1, 2, 3, 4, 5};
+
+        for(int element : numberArray){
+            System.out.println(element);
+        }
+
+        //konstruktori valja kutsumine
+        HelloWorld HelloWorld = new HelloWorld();
+
+        //kutsub valja class printsomethin
+        HelloWorld.printSomething();
+
+    }
+
+    //kui panna static ette siis ei pea seda HelloWorld.printSomething();, hellowordi ette kirjutama
+    void printSomething(){
+        for(int i = 0; i < 10; i++){
+            System.out.print(i + ",");
+        }   
     }
 }
