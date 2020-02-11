78884b2039d85c523363bf2ce2765231f8bb7935
uWookies <martten@tlu.ee>
2020-02-11 11:34:22 +0200

    teise tunni tunnitoo

---
+++ w02_klass/Character.java
@@ -0,0 +1,23 @@
+public class Character{
+
+    private int x;
+    private int y;
+
+    String slogan = "";
+
+    private String name;
+    private CharacterType characterType;
+    private Direction direction;
+
+    public Character(String name, CharacterType characterType){
+        this.name = name;
+        this.characterType = characterType;
+
+        this.x = 30;
+        this.y = 30;
+    }
+
+    public String toString(){
+        return "My name is " + name + " and i am at x: "+ x + " y: " + y + " and my slogan is " + slogan;
+    }
+}
---
+++ w02_klass/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Character mitri = new Character("Mitri", CharacterType.GOBLIN);
+
+        mitri.slogan = "hey";
+
+        System.out.println(mitri);
+
+    }
+}
